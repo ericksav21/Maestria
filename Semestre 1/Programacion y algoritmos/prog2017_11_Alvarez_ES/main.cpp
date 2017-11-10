a646fc4a0923f19fe0f448f411a21ea6f8478ffc
Erick Alvarez Valencia <erickalv21@gmail.com>
2017-11-10 14:44:33 -0600

    Tarea 11 - Programación y algoritmos: *Generación de la estructura base.

---
+++ Semestre 1/Programacion y algoritmos/prog2017_11_Alvarez_ES/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <cstdlib>
+
+#include "triage.hpp"
+
+int main(int argc, char **argv) {
+	return 0;
+}
---
+++ Semestre 1/Programacion y algoritmos/prog2017_11_Alvarez_ES/triage.hpp
@@ -0,0 +1,17 @@
+#ifndef TRIAGE_H
+#define TRIAGE_H
+
+#include <string>
+
+class Person {
+private:
+	string name;
+	double height;
+	
+public:
+	Person();
+	~Person();
+	
+};
+
+#endif
