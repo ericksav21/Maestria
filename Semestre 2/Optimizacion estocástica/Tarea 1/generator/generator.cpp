844f96e79b89d9a43354e3c38489784180c805d3
Erick Alvarez Valencia <erickalv21@gmail.com>
2018-01-30 17:09:37 -0600

    .

---
+++ Semestre 2/Optimizacion estocástica/Tarea 1/generator/generator.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <cstring>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int rand_in_range(int a, int b) {
+	return rand() % (b - a + 1) + a;
+}
+
+int main() {
+	ofstream file("tasks.txt");
+
+
+	return 0;
+}
--- Semestre 2/Optimizacion estocástica/Tarea 1/test/test.cpp
+++ Semestre 2/Optimizacion estocástica/Tarea 1/test/test.cpp
@@ -6,6 +6,7 @@
 #include <string>
 #include <ctime>
 #include <cstring>
+#include <vector>
 
 using namespace std;
 
@@ -16,8 +17,21 @@ int rand_in_range(int a, int b) {
 int main(int argc, char **argv) {
 	ofstream file;
 	srand(time(NULL));
+
+	int n = atoi(argv[1]);
 	file.open(argv[2], ofstream::out | ofstream::app);
-	file << "El parámetro es: " << string(argv[1]) << endl;
+	vector<int> v(100);
+	while(n--) {
+		v.clear();
+		for(int i = 0; i < 100; i++) {
+			v[i] = rand_in_range(1, 10);
+		}
+		int sum = 0;
+		for(int i = 0; i < 100; i++) {
+			sum += v[i];
+		}
+		file << "La suma es: " << sum << endl;
+	}
 	file.close();
 
 	return 0;
