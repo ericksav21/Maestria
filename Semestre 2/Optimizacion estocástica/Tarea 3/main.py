50f37cedaefdc01d64be199fb98edecdc8d0a6c9
Erick Alvarez Valencia <erickalv21@gmail.com>
2018-02-22 19:51:35 -0600

    .

--- Semestre 2/Optimizacion estocástica/Tarea 3/main.py
+++ Semestre 2/Optimizacion estocástica/Tarea 3/main.py
@@ -3,8 +3,8 @@ import math, sys, os
 import utils_proj as ut
 from matplotlib import pyplot as plt
 
-#path = "/home/e-082017-04/Documents/Maestria/OptEst-Tarea3/"
-path = "/home/ericksav22/Documentos/Maestria/OptEst-Tarea3/"
+path = "/home/e-082017-04/Documents/Maestria/OptEst-Tarea3/"
+#path = "/home/ericksav22/Documentos/Maestria/OptEst-Tarea3/"
 methods_dir = ["GECCO-contest"]
 
 def main():
@@ -14,14 +14,20 @@ def main():
 		return
 
 	base = sys.argv[1]
-	#ut.read_instance(os.path.join(path, methods_dir[0]), base)
-	fit, ent = ut.data_entropy(os.path.join(path, methods_dir[0]), base)
-	x1 = np.arange(1, len(fit) + 1)
-	x2, y2 = zip(*ent)
+	ent = ut.data_entropy(os.path.join(path, methods_dir[0]), base)
+	#x1 = np.arange(1, len(fit) + 1)
+	x2 = []
+	y2 = []
+	file = open(base + ".entropyEvolution", "w")
+	for k, v in ent.items():
+		x2.append(k)
+		y2.append(v / 30.0)
+		file.write(str(k) + " " + str(v / 30.0) + "\n")
 
-	print(y2)
+	file.close()
+
 	ut.make_plot(x2, y2, "entropy")
-	ut.make_plot(x1, fit, "fitness")
+	#ut.make_plot(x1, fit, "fitness")
 	'''ent = ut.precalculated_entropy(os.path.join(path, methods_dir[0]), base)
 	x, y = zip(*ent)
 	ut.make_plot(x, y, "pentropy")'''
--- Semestre 2/Optimizacion estocástica/Tarea 3/utils_proj.py
+++ Semestre 2/Optimizacion estocástica/Tarea 3/utils_proj.py
@@ -24,69 +24,120 @@ def read_instance(path, base):
 def precalculated_entropy(path, base):
 	f = open(os.path.join(path, base), "r")
 	entropy_set = []
+	is_first = True
 	while True:
 		line = f.readline()
+		if is_first:
+			is_first = False
+			continue
 		if not line:
 			break
 		t, e = line.split(" ")
 		entropy_set.append((float(t.strip()), float(e.strip())))
 	return entropy_set
+
+def data_fitness(path, base):
+	files = read_instance(path, base)
+	fitness_set = {}
+	n = 400
+	for file in files:
+		f = open(file, "r")
+		print("Fitness: " + str(f))
+		while True:
+			line = f.readline()
+			if not line or line.startswith("MonoObjective"):
+				break
+			#Leer el tiempo actual
+			if not line.startswith("Current"):
+				continue
+
+			mmax = -1
+			act_time = line.split("=")[1].strip()
+			act_time = int(act_time.split("(")[0])
+
+			line = f.readline()
+			if not line:
+				break
+			font_size = int(line.split("=")[1].strip())
+
+			for i in range(font_size):
+				line = f.readline()
+				individuals = line.split(" ")
+				fitness = int(individuals[n])
+				if fitness > mmax:
+					mmax = fitness
+
+			if act_time in fitness_set:
+				fitness_set[act_time] += mmax
+			else:
+				fitness_set[act_time] = mmax
+			f.readline()
+
+		f.close()
+
+	return fitness_set
 			
 def data_entropy(path, base):
-	f = open(os.path.join(path, base), "r")
-	entropy_set = []
-	fitness_set = []
+	files = read_instance(path, base)
+	entropy_set = {}
 	n = 400
-	while True:
-		line = f.readline()
-		if not line:
-			break
-		#Leer el tiempo actual
-		if not line.startswith("Current"):
-			continue
-
-		act_time = line.split("=")[1].strip()
-		act_time = int(act_time.split("(")[0])
-
-		line = f.readline()
-		if not line:
-			break
-		font_size = int(line.split("=")[1].strip())
-		pop = np.zeros((font_size, n), dtype = "int")
-		F = np.zeros((n, n), dtype = "float")
-
-		for i in range(font_size):
+	for file in files:
+		f = open(file, "r")
+		print("Entropía: " + str(f))
+		while True:
 			line = f.readline()
-			individuals = line.split(" ")
-			for j in range(n):
-				pop[i][j] = int(individuals[j])
-			fitness = int(individuals[n])
-			fitness_set.append(fitness)
+			if not line or line.startswith("MonoObjective"):
+				break
+			#Leer el tiempo actual
+			if not line.startswith("Current"):
+				continue
 
-		for i in range(n):
-			for j in range(font_size):
-				row = pop[j][i]
-				F[row][i] += 1
-			for j in range(n):
-				F[j][i] /= float(font_size)
+			act_time = line.split("=")[1].strip()
+			act_time = int(act_time.split("(")[0])
+
+			line = f.readline()
+			if not line:
+				break
+			font_size = int(line.split("=")[1].strip())
+			pop = np.zeros((font_size, n), dtype = "int")
+			F = np.zeros((n, n), dtype = "float")
+
+			for i in range(font_size):
+				line = f.readline()
+				individuals = line.split(" ")
+				for j in range(n):
+					pop[i][j] = int(individuals[j])
+				fitness = int(individuals[n])
+
+			for i in range(n):
+				for j in range(font_size):
+					row = pop[j][i]
+					F[row][i] += 1
+				for j in range(n):
+					F[j][i] /= float(font_size)
 
 
-		Hi = np.zeros((n), dtype = "float")
-		for i in range(n):
-			res = 0.0
-			for j in range(n):
-				if F[j][i] > 0.0:
-					res += F[j][i] * log(F[j][i], n)
-			Hi[i] = -res
+			Hi = np.zeros((n), dtype = "float")
+			for i in range(n):
+				res = 0.0
+				for j in range(n):
+					if F[j][i] > 0.0:
+						res += F[j][i] * log(F[j][i], n)
+				Hi[i] = -res
 
-		entropy_set.append((act_time, Hi.mean()))
-		#print(Hi.mean())
-		f.readline()
+			if act_time in entropy_set:
+				entropy_set[act_time] += Hi.mean()
+			else:
+				entropy_set[act_time] = Hi.mean()
+			f.readline()
 
-	return fitness_set, entropy_set
+		f.close()
 
-def make_plot(x, y, files_name):
+	return entropy_set
+
+def make_plot(x, y, files_name, title = ""):
 	plt.clf()
 	plt.figure(figsize = (7, 7))
 	plt.plot(x, y)
+	plt.title(title)
 	plt.savefig(files_name, dpi = 100)
