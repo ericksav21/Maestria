f9438891225097a9d82854f3f6d977bccb7f8812
Erick Alvarez <erickalv21@gmail.com>
2018-10-16 18:53:31 -0500

    Tarea 4 - Programación y algoritmos: *Código terminado.

--- Semestre 3/Programacion y algoritmos 2/Tarea 5/Code/main.cpp
+++ Semestre 3/Programacion y algoritmos 2/Tarea 5/Code/main.cpp
@@ -15,7 +15,8 @@
 
 using namespace std;
 
-int mat[MAXN][MAXN], dist[MAXN][MAXN];
+int dist[MAXN][MAXN];
+bool mat[MAXN][MAXN];
 int row[] = {-1, 1, 0, 0};
 int col[] = {0, 0, -1, 1};
 
@@ -45,8 +46,8 @@ void bfs() {
 
 		for(int i = 0; i < 4; i++) {
 			int x_new = x + col[i], y_new = y + row[i];
-			if(check_boundaries(y_new && x_new) &&
-				mat[y_new][x_new] == 0 &&
+			if(check_boundaries(y_new, x_new) &&
+				!mat[y_new][x_new] &&
 				dist[y_new][x_new] == INT_MAX) {
 
 				dist[y_new][x_new] = dist[y][x] + 1;
@@ -56,35 +57,82 @@ void bfs() {
 	}
 }
 
+bool is_blocked(int y, int x) {
+    int y_i, x_i;
+    bool lst = false, act = false, fst = false;
+    int outs = 0;
+    fst = lst = act = !check_boundaries(y - 1, x - 1) || mat[y - 1][x - 1];
+    act = !check_boundaries(y - 1, x) || mat[y - 1][x]; if(act != lst) outs++;
+    lst = act;
+    act = !check_boundaries(y - 1, x + 1) || mat[y - 1][x + 1]; if(act != lst) outs++;
+    lst = act;
+    act = !check_boundaries(y, x + 1) || mat[y][x + 1]; if(act != lst) outs++;
+    lst = act;
+    act = !check_boundaries(y + 1, x + 1) || mat[y + 1][x + 1]; if(act != lst) outs++;
+    lst = act;
+    act = !check_boundaries(y + 1, x) || mat[y + 1][x]; if(act != lst) outs++;
+    lst = act;
+    act = !check_boundaries(y + 1, x - 1) || mat[y + 1][x - 1]; if(act != lst) outs++;
+    lst = act;
+    act = !check_boundaries(y, x - 1) || mat[y][x - 1]; if(act != lst) outs++;
+    if(act != fst) outs++;
+
+    return outs >= 4;
+}
+
 int manhattan(int y1, int x1, int y2, int x2) {
 	return abs(x2 - x1) + abs(y2 - y1);
 }
 
-void solve(int y, int x, int nxt_cp) {
+void solve(int y, int x, int cnt, int nxt_cp) {
 	//Si se pisa la casilla final
 	if(y == 0 && x == 1) {
-		if(cnt == sz) no_paths++;
+		if(cnt == sz) {
+			no_paths++;
+		}
 		return;
 	}
 	//Hacer poda
 	//Si pisa una casilla antes de ser necesario
 	bool hit_cp = false;
 	if(nxt_cp < 3 && y == cp_r[nxt_cp] && x == cp_c[nxt_cp]) {
-		if(cnt < cp_time[nxt_cp]) return;
+		if(cnt < cp_time[nxt_cp]) {
+			return;
+		}
 		else hit_cp = true;
 	}
 	//Si no pisa la casilla en el momento exacto
-	if(nxt_cp < 3 && !hit_cp && cnt == cp_time[nxt_cp]) return;
+	if(nxt_cp < 3 && !hit_cp && cnt == cp_time[nxt_cp]) {
+		return;
+	}
 
 	//Si no pisará la casilla a tiempo
 	if(nxt_cp < 3) {
 		int md = manhattan(y, x, cp_r[nxt_cp], cp_c[nxt_cp]);
+		if(cnt + md > cp_time[nxt_cp]) {
+			return;
+		}
 	}
+	//Si se autobloqueó
+	if(is_blocked(y, x)) {
+		return;
+	}
+	/*bfs();
+	for(int i = 0; i < m; i++) {
+		for(int j = 0; j < n; j++) {
+			if(dist[i][j] == INT_MAX && !mat[i][j]) {
+				return;
+			}
+		}
+	}*/
 
 	for(int i = 0; i < 4; i++) {
 		int x_new = x + col[i], y_new = y + row[i];
-		if(!check_boundaries(y_new, x_new)) continue;
-
+		if(!check_boundaries(y_new, x_new) || mat[y_new][x_new]) continue;
+		mat[y_new][x_new] = true;
+		if(hit_cp) solve(y_new, x_new, cnt + 1, nxt_cp + 1);
+		else solve(y_new, x_new, cnt + 1, nxt_cp);
+		mat[y_new][x_new] = false;
 	}
 }
 
@@ -92,6 +140,7 @@ int main() {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
 
+	int cc = 1;
 	while(cin >> m >> n) {
 		if(m == 0 && n == 0) break;
 
@@ -99,12 +148,19 @@ int main() {
 		cp_time[0] = (int)(sz / 4);
 		cp_time[1] = (int)(sz / 2);
 		cp_time[2] = (int)(3 * sz / 4);
-		memset(mat, 0, sizeof(mat));
-		no_paths = 0;
-		cnt = 1;
+		for(int i = 0; i < m; i++) {
+			for(int j = 0; j < n; j++) {
+				mat[i][j] = false;
+			}
+		}
 		for(int i = 0; i < 3; i++) {
 			cin >> cp_r[i] >> cp_c[i];
 		}
+		no_paths = 0;
+		mat[0][0] = true;
+		solve(0, 0, 1, 0);
+		cout << "Case " << cc << ": " << no_paths << "\n";
+		cc++;
 	}
 
 	return 0;
