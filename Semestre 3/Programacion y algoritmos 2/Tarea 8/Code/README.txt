0a235bf5f3b8afe004f5a32da0daa21e8523cacf
Erick Salvador Alvarez <erickalv21@gmail.com>
2018-11-26 15:21:36 -0600

    Tarea 8 - Programación y algoritmos 2: *Tarea terminanda.

---
+++ Semestre 3/Programacion y algoritmos 2/Tarea 8/Code/README.txt
@@ -0,0 +1,7 @@
+Para compilar:
+
+g++ -O2 -o main main.cpp
+
+El programa corre según las especificaciones del juez CS Academy en el siguiente link:
+
+https://csacademy.com/contest/archive/task/squared-ends/statement/
--- Semestre 3/Programacion y algoritmos 2/Tarea 8/Code/main.cpp
+++ Semestre 3/Programacion y algoritmos 2/Tarea 8/Code/main.cpp
@@ -11,76 +11,117 @@ using namespace std;
 
 typedef long long ll;
 
-ll dp[105][MAXN], arr[MAXN];
+// DP, Arreglo de los datos y el arreglo ordenado
+ll dp[105][MAXN], arr[MAXN], dom[MAXN];
 int n, k;
 
-ll function(int k, int n, ll x) {
-	ll power = (arr[k] - x) * (arr[k] - x);
-	return dp[k - 1][n - 1] + power;
+/*
+	Estructura para manejar las funciones en el LiChao
+*/
+typedef struct _F {
+	ll dp, ak;
+
+	_F() {
+		dp = ak = 0;
+	}
+
+	_F(ll dp, ll ak) {
+		this->dp = dp;
+		this->ak = ak;
+	}
+
+	~_F() {}
+} Function;
+
+/*
+	Evalúa la función f con respecto al X dado
+*/
+ll eval_f(Function f, ll x) {
+	ll power = (f.ak - x) * (f.ak - x);
+	return f.dp + power;
 }
 
+/*
+	Clase de LiChao Tree con los métodos originales de inserción y buscado.
+*/
 class LiChao {
 private:
-	ll tree[4 * MAXN];
+	bool empty;
+	vector<Function> tree;
 
 public:
-	LiChao();
-	~LiChao();
+	LiChao() {
+		empty = true;
+		tree.resize(4 * MAXN);
+	}
+	~LiChao() {}
 	
-	void add_f(int node, int lo, int hi, int val) {
+	void add_f(int node, int lo, int hi, Function f) {
 		int mid = (lo + hi) / 2;
-		int lc = 2 * node, rc = lc + 1;
-		ll f_vl = function(val, lo);
-		ll f_vm = function(val, mid);
-		ll f_tl = function(tree[node], lo);
-		ll f_tm = function(tree[node], mid);
+		int lc = 2 * node, rc = 2 * node + 1;
 
-		bool left = f_vl < f_tl;
-		bool mid = f_vm < f_tm;
+		bool is_left = eval_f(f, dom[lo]) < eval_f(tree[node], dom[lo]);
+		bool is_mid = eval_f(f, dom[mid]) < eval_f(tree[node], dom[mid]);
 
-		if(mid) {
-			swap(tree[node], val);
+		if(is_mid) {
+			swap(tree[node], f);
 		}
-		if(r - l == 1) {
+		if(hi - lo == 1) {
 			return;
 		}
-		if(left != mid) {
-			add_f(lc, lo, mid, val);
+		if(is_left != is_mid) {
+			add_f(lc, lo, mid, f);
 		}
 		else {
-			add_f(rc, mid, hi, val);
+			add_f(rc, mid, hi, f);
 		}
 	}
 
-	ll get(int node, int lo, int hi, int val) {
-		int mid = (lo + hi) / 2;
-		int lc = 2 * node, rc = lc + 1;
+	void insert_f(Function f) {
+		// Si el árbol está vacío pon la función en todos los nodos
+		if(empty) {
+			for(int i = 0; i < tree.size(); i++) {
+				tree[i] = f;
+			}
+			empty = false;
+		}
+		else {
+			add_f(1, 0, n - 1, f);
+		}
+	}
 
-		if(r - l == 1) {
-			return function(tree[node], val);
+	ll _get(int node, int lo, int hi, ll x) {
+		int mid = (lo + hi) / 2;
+		int lc = 2 * node, rc = 2 * node + 1;
+
+		if(hi - lo == 1) {
+			return eval_f(tree[node], x);
 		}
-		if(x < mid) {
-			return min(function(tree[node], val), get(lc, lo, mid, val));
+		else if(x < dom[mid]) {
+			return min(eval_f(tree[node], x), _get(lc, lo, mid, x));
 		}
-		return min(function(tree[node], val), get(rc, mid, hi, val));
+		else {
+			return min(eval_f(tree[node], x), _get(rc, mid, hi, x));
+		}
+	}
+
+	ll get(ll x) {
+		return _get(1, 0, n - 1, x);
 	}
 };
 
 ll solve(int k, int n) {
 	for(int i = 1; i <= n; i++) {
-		dp[1][i] = (arr[i] - arr[1]) * (arr[i] - arr[1]);
+		dp[1][i] = (arr[i - 1] - arr[0]) * (arr[i - 1] - arr[0]);
 	}
-	/*for(int i = 1; i <= k; i++) {
-		dp[i][i] = 0;
-	}*/
 
-	ll power;
+	// Cálculo Bottom-Up de la tabla usando el Li Chao
 	for(int k_i = 2; k_i <= k; k_i++) {
-		for(int i = k_i; i <= n; i++) {
-			for(int j = k_i; j <= i; j++) {
-				power = (arr[i] - arr[j]) * (arr[i] - arr[j]);
-				dp[k_i][i] = min(dp[k_i][i], dp[k_i - 1][j - 1] + power);
-			}
+		LiChao arr_tree;
+		for(int i = k_i - 1; i < n; i++) {
+			Function f_act(dp[k_i - 1][i], arr[i]);
+			arr_tree.insert_f(f_act);
+			dp[k_i][i + 1] = arr_tree.get(arr[i]);
 		}
 	}
 
@@ -92,15 +133,12 @@ int main() {
 	cin.tie(0);
 
 	cin >> n >> k;
-	cout << n << " " << k << "\n";
-	for(int i = 1; i <= n; i++) {
+	for(int i = 0; i < n; i++) {
 		cin >> arr[i];
+		dom[i] = arr[i];
 	}
-	for(int i = 0; i < 105; i++) {
-		for(int j = 0; j < MAXN; j++) {
-			dp[i][j] = INF;
-		}
-	}
+	//Lo usará el Li Chao
+	sort(dom, dom + n);
 	cout << solve(k, n) << "\n";
 
 	return 0;
